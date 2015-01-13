19bd7dbc6f5e70bef519aa5c8c2488ca80079f97
Thomas Midgett <tmidge20@gmail.com>
2015-01-13 15:39:09 -0500

    Added methods

--- ruby game.rb
+++ ruby game.rb
@@ -1,10 +1,9 @@
 answer = rand(100)+1
 guess_array=[]
-guess_counter = 1
+count = 0
 
 puts "Random number game"
 puts "guess a number between 1-100"
-puts "you have 5 tries"
 
 def get_user_input
    gets.chomp.to_i
@@ -16,23 +15,29 @@ end
 
 #puts answer
 
-while guess_counter < 6
+def remaining_guesses(count)
+  suffix = (6-count==1 ? "es" :"")
+  puts "You have #{5-count} guess#{suffix} left"
+end
+
+while count < 5
+  remaining_guesses(count)
   guess = get_user_input
     if guess == answer
       puts "you got it"
       exit
     else
         if guess_array.include?(guess)
-          puts "That's the same number!"
+          puts "You've already guessed!"
         else
           if guess < answer
             puts "guess again, higher this time!"
             guess_array << guess
-            guess_counter +=1
+            count +=1
           elsif guess > answer
             puts "guess again, lower this time!"
             guess_array << guess
-            guess_counter +=1
+            count +=1
           end
         end
       end
